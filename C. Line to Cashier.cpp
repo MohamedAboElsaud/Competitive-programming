93b5ab18fb6efe6e027cb20ca443ea1710f5b92a
MohamedAboElsaud <mohamedabuelsoudtmam@gmail.com>
2021-09-20 19:21:14 +0200

    add

---
+++ C. Line to Cashier.cpp
@@ -0,0 +1,88 @@
+/*
+|||   ╔═══════════╗            ╔════╗            ╔═══════════╗    ╔═══════════╗    ╔══════════╗       |||
+|||   ║███████████║            ║████║            ║███████████║    ║███████████║    ║██████████║       |||
+|||   ║██╔════════╝           ║██║║██║           ║██╔════════╝    ║██╔════════╝    ║██══════╗██║      |||
+|||   ║██║                   ║██║╔╗║██║          ║██║             ║██║             ║██       ║██║     |||
+|||   ║██╚════════╗         ║██║╔╚╝╗║██║         ║██╚════════╗    ║██╚════════╗    ║██        ║██║    |||
+|||   ║███████████║        ║██║ ╚══╝ ║██║        ║███████████║    ║███████████║    ║██         ║██║   |||
+|||   ╚════════╗██║       ║██║██║██║██║██║       ║██╔════════╝    ║██╔════════╝    ║██        ║██║    |||
+|||            ║██║      ║██║ ╔═╔══╗═╗ ║██║      ║██║             ║██║             ║██       ║██║     |||
+|||   ╔════════╝██║     ║██║ ╔╚╔╚══╝╗╝╗ ║██║     ║██╚════════╗    ║██╚════════╗    ║██══════╝██║      |||
+|||   ║███████████║    ║██║ ╔╚╔╚════╝╗╝╗ ║██║    ║███████████║    ║███████████║    ║██████████║       |||
+|||   ╚═══════════╝    ╚══╝ ╚═╚══════╝═╝ ╚══╝    ╚═══════════╝    ╚═══════════╝    ╚══════════╝       |||
+|||                                                                                                   |||
+|||                                                                                                   |||
+|||                                  ╔════════╗  ╔════════════╗                                       |||
+|||                         ║██║     ║████████║  ║████████████║     ║██║                              |||
+|||    ║█║ ║█║            ║██║       ╚══║██║══╝  ╚════║██║════╝       ║██║            ║█║ ║█║         |||
+|||   ║█ ║█║ █║         ║██║            ║██║          ║██║              ║██║         ║█ ║█║ █║        |||
+|||    ║█   █║        ║██║              ║██║          ║██║                ║██║        ║█   █║         |||
+|||     ║█║█║           ║██║            ║██║          ║██║              ║██║           ║█║█║          |||
+|||      ║█║              ║██║       ╔══║██║══╗       ║██║            ║██║              ║█║           |||
+|||                         ║██║     ║████████║      ╔║██║╗         ║██║                              |||
+|||                                  ╚════════╝      ╚════╝                                           |||
+*/
+
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ll long long 
+#define ld long double
+
+
+using namespace std;
+void fast() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+int main()
+{
+    fast();
+
+    ll t = 0, n, l = 0, r = 0, d, mx = 0, res = 0, sum = 0,a1=0,a2=0, m, k;
+    string s;
+    map<char, ll>mp;
+   // ll n = 3, e = 3, t = 1, i = 1;
+    cin >> n;
+    ll a[30005] = { 0 };
+    set<ll>st;
+    for (ll i = 0; i < n; i++)cin >> a[i];
+    ll i = 0;
+    while (n--) {
+        sum = 0;
+        for (ll ii = 0; ii < a[i]; ii++) {
+            ll x; cin >> x;
+            sum += x;
+        }
+        st.insert((sum * 5) + a[i] * 15); i++;
+
+    }
+    cout << *st.begin();
+return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ D. Position in Fraction.cpp
@@ -0,0 +1,87 @@
+/*
+|||   ╔═══════════╗            ╔════╗            ╔═══════════╗    ╔═══════════╗    ╔══════════╗       |||
+|||   ║███████████║            ║████║            ║███████████║    ║███████████║    ║██████████║       |||
+|||   ║██╔════════╝           ║██║║██║           ║██╔════════╝    ║██╔════════╝    ║██══════╗██║      |||
+|||   ║██║                   ║██║╔╗║██║          ║██║             ║██║             ║██       ║██║     |||
+|||   ║██╚════════╗         ║██║╔╚╝╗║██║         ║██╚════════╗    ║██╚════════╗    ║██        ║██║    |||
+|||   ║███████████║        ║██║ ╚══╝ ║██║        ║███████████║    ║███████████║    ║██         ║██║   |||
+|||   ╚════════╗██║       ║██║██║██║██║██║       ║██╔════════╝    ║██╔════════╝    ║██        ║██║    |||
+|||            ║██║      ║██║ ╔═╔══╗═╗ ║██║      ║██║             ║██║             ║██       ║██║     |||
+|||   ╔════════╝██║     ║██║ ╔╚╔╚══╝╗╝╗ ║██║     ║██╚════════╗    ║██╚════════╗    ║██══════╝██║      |||
+|||   ║███████████║    ║██║ ╔╚╔╚════╝╗╝╗ ║██║    ║███████████║    ║███████████║    ║██████████║       |||
+|||   ╚═══════════╝    ╚══╝ ╚═╚══════╝═╝ ╚══╝    ╚═══════════╝    ╚═══════════╝    ╚══════════╝       |||
+|||                                                                                                   |||
+|||                                                                                                   |||
+|||                                  ╔════════╗  ╔════════════╗                                       |||
+|||                         ║██║     ║████████║  ║████████████║     ║██║                              |||
+|||    ║█║ ║█║            ║██║       ╚══║██║══╝  ╚════║██║════╝       ║██║            ║█║ ║█║         |||
+|||   ║█ ║█║ █║         ║██║            ║██║          ║██║              ║██║         ║█ ║█║ █║        |||
+|||    ║█   █║        ║██║              ║██║          ║██║                ║██║        ║█   █║         |||
+|||     ║█║█║           ║██║            ║██║          ║██║              ║██║           ║█║█║          |||
+|||      ║█║              ║██║       ╔══║██║══╗       ║██║            ║██║              ║█║           |||
+|||                         ║██║     ║████████║      ╔║██║╗         ║██║                              |||
+|||                                  ╚════════╝      ╚════╝                                           |||
+*/
+
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ll long long 
+#define ld long double
+
+
+using namespace std;
+void fast() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+int main()
+{
+    fast();
+
+    ll t = 0, n, l = 0,c, r = 0, d, mx = 0, res = 0, sum = 0, sum1 = 0, a1 = 0, a2 = 0, m, k, fr[10005] = { 0 };
+    
+    set<ll>st;
+   // ll n = 3, e = 3, t = 1, i = 1;
+  
+    cin >> n >> m>>c;
+    n = n % m;
+    while (res <= 1e6) {
+        ++res; n *= 10;
+        if (n / m == c) {
+            cout << res; return 0;
+        }
+        n =  n % m;
+    }
+
+    cout << -1;
+
+      
+return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
