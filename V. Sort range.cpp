6d9f038de012850f9c5907e631a4569d7cbcc66f
MohamedAboElsaud <mohamedabuelsoudtmam@gmail.com>
2021-10-09 19:02:14 +0200

    add

---
+++ V. Sort range.cpp
@@ -0,0 +1,96 @@
+/*
+|||   ╔═══════════╗            ╔════╗            ╔═══════════╗    ╔═══════════╗    ╔══════════╗       |||
+|||   ║███████████║            ║████║            ║███████████║    ║███████████║    ║██████████║       |||
+|||   ║██╔════════╝           ║██║║██║           ║██╔════════╝    ║██╔════════╝    ║██══════╗██║      |||
+|||   ║██║                   ║██║╔╗║██║          ║██║             ║██║             ║██       ║██║     |||
+|||   ║██╚════════╗         ║██║╔╚╝╗║██║         ║██╚════════╗    ║██╚════════╗    ║██        ║██║    |||
+|||   ║███████████║        ║██║ ╚══╝ ║██║        ║███████████║    ║███████████║    ║██         ║██║   |||
+|||   ╚════════╗██║       ║██║██║██║██║██║       ║██╔════════╝    ║██╔════════╝    ║██        ║██║    |||
+|||            ║██║      ║██║ ╔═╔══╗═╗ ║██║      ║██║             ║██║             ║██       ║██║     |||
+|||   ╔════════╝██║     ║██║ ╔╚╔╚══╝╗╝╗ ║██║     ║██╚════════╗    ║██╚════════╗    ║██══════╝██║      |||
+|||   ║███████████║    ║██║ ╔╚╔╚════╝╗╝╗ ║██║    ║███████████║    ║███████████║    ║██████████║       |||
+|||   ╚═══════════╝    ╚══╝ ╚═╚══════╝═╝ ╚══╝    ╚═══════════╝    ╚═══════════╝    ╚══════════╝       |||
+|||                                                                                                   |||
+|||                                                                                                   |||
+|||                                  ╔════════╗  ╔════════════╗                                       |||
+|||                         ║██║     ║████████║  ║████████████║     ║██║                              |||
+|||    ║█║ ║█║            ║██║       ╚══║██║══╝  ╚════║██║════╝       ║██║            ║█║ ║█║         |||
+|||   ║█ ║█║ █║         ║██║            ║██║          ║██║              ║██║         ║█ ║█║ █║        |||
+|||    ║█   █║        ║██║              ║██║          ║██║                ║██║        ║█   █║         |||
+|||     ║█║█║           ║██║            ║██║          ║██║              ║██║           ║█║█║          |||
+|||      ║█║              ║██║       ╔══║██║══╗       ║██║            ║██║              ║█║           |||
+|||                         ║██║     ║████████║      ╔║██║╗         ║██║                              |||
+|||                                  ╚════════╝      ╚════╝                                           |||
+*/
+
+// link : https://codeforces.com/group/T3p02rhrmb/contest/340610/problem/V
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ll long long int
+#define ld long double
+
+
+using namespace std;
+void fast() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+
+int main()
+{
+    fast();
+    ll t,n,m,k; cin >> t;
+    while (t--) {
+        
+       // priority_queue<ll>pq;
+        priority_queue<int, vector<int>,greater<int> > pq;
+        cin >> n >> m >> k;
+        vector<ll>a(n + 5, 0);
+        for (ll i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (ll i = 0; i <m; i++) {
+            pq.push(a[i]); 
+        }
+        ll j = 0;
+       a[j++]= pq.top();
+        pq.pop();
+        for (ll i = m; i < n; i++) {
+           pq.push(a[i]);
+           a[j++]= pq.top();
+           pq.pop();
+        }
+       while(!pq.empty()){
+            a[j++] = pq.top();
+            pq.pop();
+        }
+        cout << a[k - 1]<<"\n";
+    }
+    return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
