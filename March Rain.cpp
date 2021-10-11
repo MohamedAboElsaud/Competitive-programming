667b15d26dfcbbb507f8a6aba4ea6f5817048144
MohamedAboElsaud <mohamedabuelsoudtmam@gmail.com>
2021-10-11 22:03:22 +0200

    add

---
+++ March Rain.cpp
@@ -0,0 +1,99 @@
+
+/*
+|||   ╔═══════════╗            ╔════╗            ╔═══════════╗    ╔═══════════╗    ╔══════════╗       |||
+|||   ║███████████║            ║████║            ║███████████║    ║███████████║    ║██████████║       |||
+|||   ║██╔════════╝           ║██║║██║           ║██╔════════╝    ║██╔════════╝    ║██══════╗██║      |||
+|||   ║██║                   ║██║╔╗║██║          ║██║             ║██║             ║██       ║██║     |||
+|||   ║██╚════════╗         ║██║╔╚╝╗║██║         ║██╚════════╗    ║██╚════════╗    ║██        ║██║    |||
+|||   ║███████████║        ║██║ ╚══╝ ║██║        ║███████████║    ║███████████║    ║██         ║██║   |||
+|||   ╚════════╗██║       ║██║██║██║██║██║       ║██╔════════╝    ║██╔════════╝    ║██        ║██║    |||
+|||            ║██║      ║██║ ╔═╔══╗═╗ ║██║      ║██║             ║██║             ║██       ║██║     |||
+|||   ╔════════╝██║     ║██║ ╔╚╔╚══╝╗╝╗ ║██║     ║██╚════════╗    ║██╚════════╗    ║██══════╝██║      |||
+|||   ║███████████║    ║██║ ╔╚╔╚════╝╗╝╗ ║██║    ║███████████║    ║███████████║    ║██████████║       |||
+|||   ╚═══════════╝    ╚══╝ ╚═╚══════╝═╝ ╚══╝    ╚═══════════╝    ╚═══════════╝    ╚══════════╝       |||
+|||                                                                                                   |||
+|||                                                                                                   |||
+|||                                  ╔════════╗  ╔════════════╗                                       |||
+|||                         ║██║     ║████████║  ║████████████║     ║██║                              |||
+|||    ║█║ ║█║            ║██║       ╚══║██║══╝  ╚════║██║════╝       ║██║            ║█║ ║█║         |||
+|||   ║█ ║█║ █║         ║██║            ║██║          ║██║              ║██║         ║█ ║█║ █║        |||
+|||    ║█   █║        ║██║              ║██║          ║██║                ║██║        ║█   █║         |||
+|||     ║█║█║           ║██║            ║██║          ║██║              ║██║           ║█║█║          |||
+|||      ║█║              ║██║       ╔══║██║══╗       ║██║            ║██║              ║█║           |||
+|||                         ║██║     ║████████║      ╔║██║╗         ║██║                              |||
+|||                                  ╚════════╝      ╚════╝                                           |||
+*/
+
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ll long long int
+#define ld long double
+
+
+using namespace std;
+void fast() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+ll n, k,c,a[200005],cnt,pos;
+bool chk(ll x) {
+    pos = 0, cnt = 0;
+    for (ll i = 1; i <= n; i++) {
+        if (pos < a[i]) { 
+            cnt++;
+            pos = a[i] + x - 1;
+            if (k == cnt) {
+                 return (pos >= a[n])? true:false;
+                 
+            }
+            if (pos >= a[n]) return true ;
+        }
+
+    }
+    return false;
+}
+int main()
+{
+    fast();
+    ll t; cin >> t;
+    while (t--) {
+        cin >> n >> k;
+        for (ll i = 1; i <= n; i++)cin >> a[i];
+        ll md, l = 1, r = a[n];
+        while (l <= r) {
+            md = l + (r - l) / 2;
+          
+            if (chk(md))r= md - 1;
+            else l = md + 1;
+        }
+        cout << l<<"\n";
+    }
+    return 0;
+}
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
