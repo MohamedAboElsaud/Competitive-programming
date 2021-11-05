1d2fc9f31b4f18b39a24e747771df7fae3f269b4
MohamedAboElsaud <mohamedabuelsoudtmam@gmail.com>
2021-11-05 15:54:36 +0200

    add

---
+++ Center Alignment.cpp
@@ -0,0 +1,93 @@
+
+/*
+|||   ╔═══════════╗            ╔════╗            ╔═══════════╗    ╔═══════════╗    ╔══════════╗       |||
+|||   ║███████████║            ║████║            ║███████████║    ║███████████║    ║██████████║       |||
+|||   ║██╔════════╝           ║██║║██║           ║██╔════════╝    ║██╔════════╝    ║██══════╗██║      |||
+|||   ║██║                   ║██║╔╗║██║          ║██║             ║██║             ║██       ║██║     |||
+|||   ║██╚════════╗         ║██║╔╚╝╗║██║         ║██╚════════╗    ║██╚════════╗    ║██        ║██║    |||
+|||   ║███████████║        ║██║ ╚══╝ ║██║        ║███████████║    ║███████████║    ║██         ║██║   |||
+|||   ╚════════╗██║       ║██║██║██║██║██║       ║██╔════════╝    ║██╔════════╝    ║██        ║██║    |||
+|||            ║██║      ║██║ ╔═╔══╗═╗ ║██║      ║██║             ║██║             ║██       ║██║     |||
+|||   ╔════════╝██║     ║██║ ╔╚╔╚══╝╗╝╗ ║██║     ║██╚════════╗    ║██╚════════╗    ║██══════╝██║      |||
+|||   ║███████████║    ║██║ ╔╚╔╚════╝╗╝╗ ║██║    ║███████████║    ║███████████║    ║██████████║       |||
+|||   ╚═══════════╝    ╚══╝ ╚═╚══════╝═╝ ╚══╝    ╚═══════════╝    ╚═══════════╝    ╚══════════╝       |||
+|||                                                                                                   |||
+|||                                                                                                   |||
+|||                                  ╔════════╗  ╔════════════╗                                       |||
+|||                         ║██║     ║████████║  ║████████████║     ║██║                              |||
+|||    ║█║ ║█║            ║██║       ╚══║██║══╝  ╚════║██║════╝       ║██║            ║█║ ║█║         |||
+|||   ║█ ║█║ █║         ║██║            ║██║          ║██║              ║██║         ║█ ║█║ █║        |||
+|||    ║█   █║        ║██║              ║██║          ║██║                ║██║        ║█   █║         |||
+|||     ║█║█║           ║██║            ║██║          ║██║              ║██║           ║█║█║          |||
+|||      ║█║              ║██║       ╔══║██║══╗       ║██║            ║██║              ║█║           |||
+|||                         ║██║     ║████████║      ╔║██║╗         ║██║                              |||
+|||                                  ╚════════╝      ╚════╝                                           |||
+*/
+
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ll long long int
+#define ld long double
+
+//link : https://codeforces.com/group/u3Ii79X3NY/contest/270796/problem/N
+
+using namespace std;
+void fast() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+string s;
+vector<string> v;
+int fr[1005][1005] = { 0 };
+int main()
+{
+    fast();
+    ll mx = 0,i,k,n=1; string x;
+    queue<string>ss;
+    while (getline(cin, x)) {
+        ss.push(x);
+        mx = max(mx, (ll)(x.length()));
+    }
+    for (i = 0; i < mx+2; i++)cout << '*';
+   cout <<endl;
+   while(!ss.empty()){
+       cout << '*';
+       if (mx % 2 != ss.front().size() % 2) {
+           n = -n;  
+       for (i = 0; i < (mx - ss.front().size() + n) / 2; i++)cout << ' ';
+       cout << ss.front();
+       for (i = 0; i < (mx - ss.front().size() - n) / 2; i++)cout << ' ';
+       
+       }
+       else {
+           for (i = 0; i < (mx - ss.front().size()) / 2; i++)cout << ' ';
+           cout << ss.front();
+           for (i = 0; i < (mx - ss.front().size()) / 2; i++)cout << ' ';
+       }
+    
+       cout << '*';
+       cout << endl;
+       ss.pop();
+   }
+   for (i = 0; i < mx + 2; i++)cout << '*';
+   cout << endl;
+
+
+    return 0;
+}
