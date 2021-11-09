7c960998afbf2ef61853712015ee15e541ce77db
MohamedAboElsaud <mohamedabuelsoudtmam@gmail.com>
2021-11-09 23:14:56 +0200

    add

---
+++ Cd and pwd commands.cpp
@@ -0,0 +1,97 @@
+
+/*
+|||   ╔═══════════╗            ╔════╗            ╔═══════════╗    ╔═══════════╗    ╔══════════╗       |||
+|||   ║███████████║            ║████║            ║███████████║    ║███████████║    ║██████████║       |||
+|||   ║██╔════════╝           ║██║║██║           ║██╔════════╝    ║██╔════════╝    ║██══════╗██║      |||
+|||   ║██║                   ║██║╔╗║██║          ║██║             ║██║             ║██       ║██║     |||
+|||   ║██╚════════╗         ║██║╔╚╝╗║██║         ║██╚════════╗    ║██╚════════╗    ║██        ║██║    |||
+|||   ║███████████║        ║██║ ╚══╝ ║██║        ║███████████║    ║███████████║    ║██         ║██║   |||
+|||   ╚════════╗██║       ║██║██║██║██║██║       ║██╔════════╝    ║██╔════════╝    ║██        ║██║    |||
+|||            ║██║      ║██║ ╔═╔══╗═╗ ║██║      ║██║             ║██║             ║██       ║██║     |||
+|||   ╔════════╝██║     ║██║ ╔╚╔╚══╝╗╝╗ ║██║     ║██╚════════╗    ║██╚════════╗    ║██══════╝██║      |||
+|||   ║███████████║    ║██║ ╔╚╔╚════╝╗╝╗ ║██║    ║███████████║    ║███████████║    ║██████████║       |||
+|||   ╚═══════════╝    ╚══╝ ╚═╚══════╝═╝ ╚══╝    ╚═══════════╝    ╚═══════════╝    ╚══════════╝       |||
+|||                                                                                                   |||
+|||                                                                                                   |||
+|||                                  ╔════════╗  ╔════════════╗                                       |||
+|||                         ║██║     ║████████║  ║████████████║     ║██║                              |||
+|||    ║█║ ║█║            ║██║       ╚══║██║══╝  ╚════║██║════╝       ║██║            ║█║ ║█║         |||
+|||   ║█ ║█║ █║         ║██║            ║██║          ║██║              ║██║         ║█ ║█║ █║        |||
+|||    ║█   █║        ║██║              ║██║          ║██║                ║██║        ║█   █║         |||
+|||     ║█║█║           ║██║            ║██║          ║██║              ║██║           ║█║█║          |||
+|||      ║█║              ║██║       ╔══║██║══╗       ║██║            ║██║              ║█║           |||
+|||                         ║██║     ║████████║      ╔║██║╗         ║██║                              |||
+|||                                  ╚════════╝      ╚════╝                                           |||
+*/
+
+// link : https://codeforces.com/group/u3Ii79X3NY/contest/270796/problem/X
+
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <iomanip>
+#include <string>
+#include <stack>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <set>
+#include <map>
+#include <iomanip>
+#include <fstream>
+#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define ll long long int
+#define ld long double
+
+
+using namespace std;
+void fast() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+}
+
+int fr[1005][1005] = { 0 };
+int main()
+{
+    fast();
+    ll n, q, i, x = 0, t; cin >> t;
+    deque<string>d; string s;
+
+    while (t--) {
+        cin >> s;
+        if (s == "pwd") {
+            cout << '/';
+            for (i = 0; i < d.size(); i++) {
+                cout << d[i] << '/';
+            }
+            cout << '\n';
+       }
+        else if (s == "cd") {
+            string ss; cin >> s; i = 0;
+            if (s[i] == '/') {
+                while (!d.empty())d.pop_back();
+                i++;
+            }
+            for (; i < s.size(); i++) {
+                if (isalpha( s[i])) {
+                 ss +=s[i];
+                }
+                else if (s[i] == '.') {
+                    if (!d.empty())d.pop_back();
+                  i++;  ss = "";
+                }
+                else if(ss.size()){
+                    d.push_back(ss); ss = "";
+                }
+            }
+            if(ss.size()) d.push_back(ss), ss = "";
+        }
+
+
+        
+    }
+    return 0;
+}
