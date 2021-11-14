4b81a152d6234fb29d4c7ef3aeebc32932759aca
MohamedAboElsaud <mohamedabuelsoudtmam@gmail.com>
2021-11-14 19:14:04 +0200

    add

--- Less or Equal.cpp
+++ Less or Equal.cpp
@@ -24,6 +24,7 @@
 |||                                  ╚════════╝      ╚════╝                                           |||
 */
 
+// link : https://codeforces.com/group/u3Ii79X3NY/contest/272220/problem/B
 
 #include <iostream>
 #include <cmath>
